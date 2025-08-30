77bb23089f74ca52b1ce6983eb1b184e287c5a1c
unknown <senthilrajagit>
2025-08-30 12:15:52 +0530

    setting DB config Changes

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',  # The name of your PostgreSQL database
-        'USER': 'postgres',  # The username to connect to the database
-        'PASSWORD': 'postgres',  # The password for the database user
-        'HOST': '127.0.0.1',  # The host where your PostgreSQL server is running (e.g., 'localhost' or an IP address)
-        'PORT': '5432',  # The port PostgreSQL is listening on (default is '5432')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ecommerce',  # The name of your PostgreSQL database
+#         'USER': 'postgres',  # The username to connect to the database
+#         'PASSWORD': 'postgres',  # The password for the database user
+#         'HOST': '127.0.0.1',  # The host where your PostgreSQL server is running (e.g., 'localhost' or an IP address)
+#         'PORT': '5432',  # The port PostgreSQL is listening on (default is '5432')
+#     }
+# }
 
 
 # Password validation
